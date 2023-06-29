53f474e29f5979e441a1cc7f427156f2f1189478
mimcclure <mmcclure2001@gmail.com>
2023-06-28 22:08:19 -0400

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "ParkMe",
+    subtitle: "C#, ASP.NET Core MVC, HTML, CSS, SQL Server",
+    description:
+      "A parking app for Georgia State University students and faculty.",
+    //image: "./project-1.png",
+    link: "https://github.com/CSC-4350-TR-SP2023/Team5",
+  },
+  {
+    title: "MangaBase",
+    subtitle: "React, HTML, CSS, SCSS, PHP, phpMyAdmin, AWS, Netlify",
+    description:
+      "A web application that displays manga using the Jikan API, with the ability to filter based on search.",
+    link: "http://54.172.150.247/wp-content/react/src/login.php",
+  },
+  {
+    title: "ASHRAE – Great Energy Predictor III",
+    subtitle: "Python(Jupyter)",
+    description:
+      "Developed accurate models of metered building energy usage in the following areas: chilled water, eletric, hot water, and steam meters.",
+    link: "https://github.com/mimcclure/Datamining_spring2022_project",
+  },
+  {
+    title: "GitHub API Integration and Data Retrieval",
+    subtitle: "Python",
+    description:
+      "Developed a script that pulls and displays a GitHub user's data",
+    link: "https://github.com/mimcclure/GitHubAPIendpoint/tree/master",
+  },
+];
+
+export const skills = [
+  "Python",
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "Go",
+  "C#",
+  "Docker",
+  "AWS",
+  "GCP"
+];
